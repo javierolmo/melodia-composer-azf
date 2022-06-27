efc028f62eadee0ad0ca4b818177e3e4dca7f34a
Javier Olmo Injerto <44966721+javierolmo@users.noreply.github.com>
2022-06-27 15:40:38 +0200

    Added Event

--- src/main/java/com/javi/uned/melodiacomposerazf/EventGridFunction.java
+++ src/main/java/com/javi/uned/melodiacomposerazf/EventGridFunction.java
@@ -1,5 +1,6 @@
 package com.javi.uned.melodiacomposerazf;
 
+import com.javi.uned.melodiacomposerazf.services.Event;
 import com.microsoft.azure.functions.ExecutionContext;
 import com.microsoft.azure.functions.annotation.EventGridTrigger;
 import com.microsoft.azure.functions.annotation.FunctionName;
@@ -8,11 +9,12 @@ public class EventGridFunction {
 
     @FunctionName("composition-request")
     public String event(
-            @EventGridTrigger(name = "specs") String specsJson,
+            @EventGridTrigger(name = "specs") Event<String> specsJson,
             final ExecutionContext executionContext
     ) {
         executionContext.getLogger().info("Java EventGrid trigger processed a request.");
         executionContext.getLogger().info("Specs JSON: " + specsJson);
-        return specsJson;
+        
+        return specsJson.getData();
     }
 }
---
+++ src/main/java/com/javi/uned/melodiacomposerazf/services/Event.java
@@ -0,0 +1,91 @@
+package com.javi.uned.melodiacomposerazf.services;
+
+public class Event <T>{
+
+    private String id;
+    private String subject;
+    private T data;
+    private String eventType;
+    private String dataVersion;
+    private String metadataVersion;
+    private String eventTime;
+    private String topic;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public String getEventType() {
+        return eventType;
+    }
+
+    public void setEventType(String eventType) {
+        this.eventType = eventType;
+    }
+
+    public String getDataVersion() {
+        return dataVersion;
+    }
+
+    public void setDataVersion(String dataVersion) {
+        this.dataVersion = dataVersion;
+    }
+
+    public String getMetadataVersion() {
+        return metadataVersion;
+    }
+
+    public void setMetadataVersion(String metadataVersion) {
+        this.metadataVersion = metadataVersion;
+    }
+
+    public String getEventTime() {
+        return eventTime;
+    }
+
+    public void setEventTime(String eventTime) {
+        this.eventTime = eventTime;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    @Override
+    public String toString() {
+        return "Event{" +
+                "id='" + id + '\'' +
+                ", subject='" + subject + '\'' +
+                ", data=" + data +
+                ", eventType='" + eventType + '\'' +
+                ", dataVersion='" + dataVersion + '\'' +
+                ", metadataVersion='" + metadataVersion + '\'' +
+                ", eventTime='" + eventTime + '\'' +
+                ", topic='" + topic + '\'' +
+                '}';
+    }
+}
