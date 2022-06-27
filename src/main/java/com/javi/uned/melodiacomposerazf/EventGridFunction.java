9ea6b53b144beda7d6dfe5604b0dfb16c70a4c56
Javier Olmo Injerto <44966721+javierolmo@users.noreply.github.com>
2022-06-27 15:47:02 +0200

    Update EventGridFunction.java

--- src/main/java/com/javi/uned/melodiacomposerazf/EventGridFunction.java
+++ src/main/java/com/javi/uned/melodiacomposerazf/EventGridFunction.java
@@ -1,5 +1,6 @@
 package com.javi.uned.melodiacomposerazf;
 
+import com.javi.uned.melodiacomposerazf.domain.SpecsDTO;
 import com.javi.uned.melodiacomposerazf.services.Event;
 import com.microsoft.azure.functions.ExecutionContext;
 import com.microsoft.azure.functions.annotation.EventGridTrigger;
@@ -9,12 +10,12 @@ public class EventGridFunction {
 
     @FunctionName("composition-request")
     public String event(
-            @EventGridTrigger(name = "specs") Event<String> specsJson,
+            @EventGridTrigger(name = "specs") Event<SpecsDTO> specsEvent,
             final ExecutionContext executionContext
     ) {
         executionContext.getLogger().info("Java EventGrid trigger processed a request.");
-        executionContext.getLogger().info("Specs JSON: " + specsJson);
-        
-        return specsJson.getData();
+        executionContext.getLogger().info("Specs JSON: " + specsEvent);
+
+        return specsEvent.getData().toString();
     }
 }
