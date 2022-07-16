bf4ba45c4e087bf76980368ffefecb7ff511795e
Javi <olmo.injerto.javier@gmail.com>
2022-07-16 13:29:50 +0200

    Create RequestEntity.java

---
+++ src/main/java/com/javi/uned/melodiacomposerazf/domain/RequestEntity.java
@@ -0,0 +1,72 @@
+package com.javi.uned.melodiacomposerazf.domain;
+
+import java.time.LocalDateTime;
+
+public class RequestEntity {
+
+    private Long id;
+    private Long userId;
+    private LocalDateTime startDateTime;
+    private LocalDateTime endDateTime;
+    private String azfCode;
+    private String specs;
+    private String status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public LocalDateTime getStartDateTime() {
+        return startDateTime;
+    }
+
+    public void setStartDateTime(LocalDateTime startDateTime) {
+        this.startDateTime = startDateTime;
+    }
+
+    public LocalDateTime getEndDateTime() {
+        return endDateTime;
+    }
+
+    public void setEndDateTime(LocalDateTime endDateTime) {
+        this.endDateTime = endDateTime;
+    }
+
+    public String getAzfCode() {
+        return azfCode;
+    }
+
+    public void setAzfCode(String azfCode) {
+        this.azfCode = azfCode;
+    }
+
+    public String getSpecs() {
+        return specs;
+    }
+
+    public void setSpecs(String specs) {
+        this.specs = specs;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+
+}
